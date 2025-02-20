276e8a685d82d128b9757915f94a78aa685ea1c6
addy <addyyy118@gmail.com>
2025-02-20 21:34:04 +0530

    feat: add dummy users

--- prisma/index.js
+++ prisma/index.js
@@ -1,8 +1,77 @@
 const db = require("../config/prismaClient");
 
 const main = async () => {
-  // await db.user.deleteMany();
-  // console.log("all entries in model deleted!");
+  // const users = await db.user.createMany({
+  //   data: [
+  //     {
+  //       name: "Addy",
+  //       email: "adi@howarts.edu",
+  //       password:
+  //         "$2a$10$n2Hn4GchZIheqhqUHy/7tua/cN5nJE3b80erbOUoquqRxtIzx2Hbq",
+  //     },
+  //     {
+  //       name: "Hermione",
+  //       email: "gran@howarts.edu",
+  //       password:
+  //         "$2a$10$eAWPnAPIf143PLm7A/NLa.ff5pFkkqRv8uF58EWz8pwtXNSz4ZwE2",
+  //     },
+  //     {
+  //       name: "Harry",
+  //       email: "harry@howarts.edu",
+  //       password:
+  //         "$2a$10$VGRETxSz86cUZrzkvrUqeOkh5q50ctecNnWAeMvXyFj5DunyFL4Dy",
+  //     },
+  //     {
+  //       name: "Ron",
+  //       email: "ron@howarts.edu",
+  //       password:
+  //         "$2a$10$tel7G/ga.B/Pakl55jLm8u5ctCgqGTID397.tZdjhNNYbkAyU.sUS",
+  //     },
+  //     {
+  //       name: "Snape",
+  //       email: "snape@howarts.edu",
+  //       password:
+  //         "$2a$10$n2Hn4GchZIheqhqUHy/7tua/cN5nJE3b80erbOUoquqRxtIzx2Hbq",
+  //     },
+  //     {
+  //       name: "Dumbledore",
+  //       email: "dumble@howarts.edu",
+  //       password:
+  //         "$2a$10$Gla7yuPWqiXdhooKpJfrFudrcNAYDYw5BEVzSxc6zp/1y77AviOcm",
+  //     },
+  //     {
+  //       name: "Voldemort",
+  //       email: "voldy@howarts.edu",
+  //       password:
+  //         "$2a$10$KDOUsEJHfPHhqW8htALpVOicw/42KquNDg0r.GqgWwPmAQLQwC7/q",
+  //     },
+  //     {
+  //       name: "Draco",
+  //       email: "draco@howarts.edu",
+  //       password:
+  //         "$2a$10$9j9tlCJdnU8JKlGQN5ieveIbegVh.8eqaIpesHjCsLF0NT3jonZMG",
+  //     },
+  //     {
+  //       name: "Hagrid",
+  //       email: "hagrid@howarts.edu",
+  //       password:
+  //         "$2a$10$Ciz5EZ/j1AaOwncxutstJ.tPgI2g0QQQRlrVsXaCKCzbG3klIC3JC",
+  //     },
+  //     {
+  //       name: "Sirius",
+  //       email: "black@howarts.edu",
+  //       password:
+  //         "$2a$10$z2IB7HAbmvXwlh4DibAwDeQpG8Tp3tLhvZCfpaY8QCNT3CMK164e6",
+  //     },
+  //     {
+  //       name: "McGonagall",
+  //       email: "minerva@howarts.edu",
+  //       password:
+  //         "$2a$10$NYTzAs1dI9.Uth1qvQAC0.RDNf8SeUoF1sKq9ogBBLKgz86axJnPa",
+  //     },
+  //   ],
+  // });
+  // console.log(users);
 };
 
 main();
---
+++ users.js
@@ -0,0 +1,58 @@
+pass = "1235678";
+[
+  {
+    name: "Addy",
+    email: "adi@hogwarts.edu",
+    password: "$2a$10$n2Hn4GchZIheqhqUHy/7tua/cN5nJE3b80erbOUoquqRxtIzx2Hbq",
+  },
+  {
+    name: "Hermione",
+    email: "gran@hogwarts.edu",
+    password: "$2a$10$eAWPnAPIf143PLm7A/NLa.ff5pFkkqRv8uF58EWz8pwtXNSz4ZwE2",
+  },
+  {
+    name: "Harry",
+    email: "harry@hogwarts.edu",
+    password: "$2a$10$VGRETxSz86cUZrzkvrUqeOkh5q50ctecNnWAeMvXyFj5DunyFL4Dy",
+  },
+  {
+    name: "Ron",
+    email: "ron@hogwarts.edu",
+    password: "$2a$10$tel7G/ga.B/Pakl55jLm8u5ctCgqGTID397.tZdjhNNYbkAyU.sUS",
+  },
+  {
+    name: "Snape",
+    email: "snape@hogwarts.edu",
+    password: "$2a$10$n2Hn4GchZIheqhqUHy/7tua/cN5nJE3b80erbOUoquqRxtIzx2Hbq",
+  },
+  {
+    name: "Dumbledore",
+    email: "dumble@hogwarts.edu",
+    password: "$2a$10$Gla7yuPWqiXdhooKpJfrFudrcNAYDYw5BEVzSxc6zp/1y77AviOcm",
+  },
+  {
+    name: "Voldemort",
+    email: "voldy@hogwarts.edu",
+    password: "$2a$10$KDOUsEJHfPHhqW8htALpVOicw/42KquNDg0r.GqgWwPmAQLQwC7/q",
+  },
+  {
+    name: "Draco",
+    email: "draco@hogwarts.edu",
+    password: "$2a$10$9j9tlCJdnU8JKlGQN5ieveIbegVh.8eqaIpesHjCsLF0NT3jonZMG",
+  },
+  {
+    name: "Hagrid",
+    email: "hagrid@hogwarts.edu",
+    password: "$2a$10$Ciz5EZ/j1AaOwncxutstJ.tPgI2g0QQQRlrVsXaCKCzbG3klIC3JC",
+  },
+  {
+    name: "Sirius",
+    email: "black@hogwarts.edu",
+    password: "$2a$10$z2IB7HAbmvXwlh4DibAwDeQpG8Tp3tLhvZCfpaY8QCNT3CMK164e6",
+  },
+  {
+    name: "McGonagall",
+    email: "minerva@hogwarts.edu",
+    password: "$2a$10$NYTzAs1dI9.Uth1qvQAC0.RDNf8SeUoF1sKq9ogBBLKgz86axJnPa",
+  },
+];
